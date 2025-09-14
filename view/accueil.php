23359c6a309cb2b6bfe7180a752bb2269824909b
Kristof-sZn <siazon.jeanchristophe.pro@gmail.com>
2025-09-14 22:40:12 +0200

    modification de la redirection pour la page des reservation(ajoutReserv.php + suppression de la page reservation.php

--- view/accueil.php
+++ view/accueil.php
@@ -3,5 +3,5 @@
 
 <nav>
 			<button onclick="window.location.href='view/vehicule/vehicule.php';" name="button"><strong>Liste des Véhicules</strong></button>
-				<button onclick="window.location.href='view/reservation/reservation.php';" name="button"><strong>Réservation</strong></button>
+				<button onclick="window.location.href='view/reservation/ajoutReserv.php';" name="button"><strong>Réservation</strong></button>
 		 </nav>
