643db7d47b7aecf0a3a7cd977be493cd5548aa7d
Kristof-sZn <siazon.jeanchristophe.pro@gmail.com>
2025-09-14 23:41:10 +0200

    modif. du model: vehiculeModel.php

--- model/vehiculeModel.php
+++ model/vehiculeModel.php
@@ -1,18 +1,15 @@
 <?php
-require_once('bdd/bdd.php');
+require_once(__DIR__ . '/../bdd/bdd.php');
 
-// Récupérer tous les véhicules
 function getVehicules() {
-    global $pdo;
-    $sql = "SELECT * FROM Vehicule";
-    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    global $bdd;
+    return $bdd->query("SELECT * FROM Vehicule")->fetchAll(PDO::FETCH_ASSOC);
 }
 
-// Récupérer un véhicule par son ID
 function getVehiculeById($id) {
-    global $pdo;
+    global $bdd;
     $sql = "SELECT * FROM Vehicule WHERE Id_Vehicule = ?";
-    $stmt = $pdo->prepare($sql);
+    $stmt = $bdd->prepare($sql);
     $stmt->execute([$id]);
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
