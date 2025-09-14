70019970edafc4c948d7d1887d74907fed7bd9aa
youyoux.debug <y.boumlik93@gmail.com>
2025-09-14 12:53:13 +0200

    Mise à jour des fichiers

--- view/accueil.php
+++ view/accueil.php
@@ -1,1 +1,6 @@
 <H1>Bonjour</H1>
+
+<nav>
+			<button onclick="window.location.href='index.html';" name="button"><strong>Accueil</strong></button>
+				<button onclick="window.location.href='';" name="button"><strong>Contact</strong></button>
+		 </nav>
