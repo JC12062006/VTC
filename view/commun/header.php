c808e49e22acde1a54a00fcce0c2293f5ddd43bd
youyoux.debug <y.boumlik93@gmail.com>
2025-09-14 19:00:34 +0200

    Mise à jour des fichiers

--- view/commun/header.php
+++ view/commun/header.php
@@ -7,6 +7,7 @@
         <meta name="description" content="">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="public/css/styles.css">
+        <link rel="stylesheet" type="text/css" href="public/css/styles.css"></center>
+        <center><a href="index.php"><img class="displayed" src="public/img/logo.png" alt="taxiloco"></a>
     </head>
     <body>
--- view/vehicule/vehicule.php
+++ view/vehicule/vehicule.php
@@ -1,19 +1,12 @@
-<?php 
-echo "Chemin actuel : " . __DIR__ . "<br>";
-echo "Fichier actuel : " . __FILE__ . "<br>";
-?>
-
 <h1>Voici la liste des Véhicules</h1>
 
 <!-- debut ajout de la liste des types -->
 <?php 
-echo "Tentative d'inclusion de selectVehicule.php...<br>";
 include('../../controller/vehicule/selectVehicule.php'); 
-echo "Inclusion terminée<br>";
 
 // Vérifier si la variable existe
 if (isset($vehicules)) {
-    echo "Variable \$vehicules définie avec " . count($vehicules) . " éléments<br>";
+    echo "L'entreprise VTC vous donne le choix avec " . count($vehicules) . " types de véhicules<br>";
 } else {
     echo "Variable \$vehicules NON définie<br>";
 }
