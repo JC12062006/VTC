5f906121a5d0299c84a2c7c9851c8266d2243f3d
youyoux.debug <y.boumlik93@gmail.com>
2025-09-14 16:51:28 +0200

    Mise à jour des fichiers

--- controller/reservation/selectReservation.php
+++ controller/reservation/selectReservation.php
@@ -2,7 +2,7 @@
 
 include("bdd/bdd.php");
 
-$req = $bdd->prepare("SELECT * FROM reservation.Id_Reservation, vehicule.Marque from reservation, chauffeur.Nom from reservation, reservation.Lieu, reservation.Date_Reservation from tache, vehicule where reservation.Id_Vehicule = vehicule.id, chauffeur where reservation.Id_Chauffeur = chauffeur.id ORDER by Date_Reservation DESC;");
+$req = $bdd->prepare("SELECT * FROM reservation.Id_Reservation, vehicule.Marque from reservation, chauffeur.Nom from reservation, reservation.Lieu, reservation.Date_Reservation, vehicule where reservation.Id_Vehicule = vehicule.id, chauffeur where reservation.Id_Chauffeur = chauffeur.id ORDER by Date_Reservation DESC;");
 $req->execute();
 
 $reservations = $req->fetchAll();
