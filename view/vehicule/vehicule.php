b1908b297ee7c86484014164c7537872fef01335
youyoux.debug <y.boumlik93@gmail.com>
2025-09-14 17:38:12 +0200

    Mise à jour des fichiers

--- view/vehicule/vehicule.php
+++ view/vehicule/vehicule.php
@@ -1,6 +1,21 @@
 <h1>Voici la liste des Véhicules</h1>
 
+<?php 
+echo "Chemin actuel : " . __DIR__ . "<br>";
+echo "Fichier actuel : " . __FILE__ . "<br>";
+?>
+
 <!-- debut ajout de la liste des types -->
 <?php include('../../controller/vehicule/selectVehicule.php');  ?>
+
+<!-- Debug : vérifier si $vehicules contient des données -->
+<?php 
+if (empty($vehicules)) {
+    echo "<p>Aucun véhicule trouvé dans la base de données.</p>";
+} else {
+    echo "<p>Nombre de véhicules trouvés : " . count($vehicules) . "</p>";
+}
+?>
+
 <?php  include('vehiculeList.php'); ?>
 <!-- fin ajout de la liste des types -->
